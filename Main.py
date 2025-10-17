f7e8df2aa6942f20e257fc8ee4af4ee13c4b9602
Skyler <schoplin01@gmail.com>
2025-10-16 20:14:09 -0400

    Switched to make_moons

--- Main.py
+++ Main.py
@@ -1,18 +1,21 @@
 import tensorflow as tf
-from sklearn.datasets import make_classification
+#from sklearn.datasets import make_classification
+from sklearn.datasets import make_moons
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
 
-X, y = make_classification(
-    n_samples=1000,
-    n_features=2,
-    n_informative=2,
-    n_redundant=0,
-    n_repeated=0,
-    n_classes=2,
-    random_state=42
-)
+#X, y = make_classification(
+#    n_samples=1000,
+#    n_features=2,
+#    n_informative=2,
+#    n_redundant=0,
+#    n_repeated=0,
+#    n_classes=2,
+ #   random_state=42
+#)
+
+X, y = make_moons(n_samples=1000, noise=0.2, random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
